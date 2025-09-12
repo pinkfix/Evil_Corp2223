70477548dbbc110b11576b8de98aa07d24cacb33
itggot-emil-willbas <emil@vildbas.com>
2025-09-12 10:43:30 +0200

    update25
    updaterat lite 25

--- app.rb
+++ app.rb
@@ -3,27 +3,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
-get('/') do
-  @dogs = [
-    {
-      name: "Fido",
-      age: "7"
-    },
-    {
-      name: "Snobben",
-      age: "329"
-    }
-  ]
-  slim(:info)
-end
-
 get('/evil_corp') do
-  #Strunta i vad som händer på rad 21-24 så länge.
+  #Strunta i vad som händer på rad 8-10 så länge.
   db = SQLite3::Database.new("db/employees.db")
   db.results_as_hash = true #Få svar i strukturen [{},{},{}]
   @data = db.execute("SELECT * FROM employees")
   
   p @data
-
+  
   slim(:evil_corp)
 end
